bb8db3a565818b6ee3a5009cba426fc4afa5eede
dannyjoo7 <danny_joo@naver.com>
2023-09-26 10:18:26 +0900

    DESIGN: 채널 UI 변경

--- app/src/main/java/com/example/mymedia/home/adapter/HomeVideoListAdapter.kt
+++ app/src/main/java/com/example/mymedia/home/adapter/HomeVideoListAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.mymedia.home
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.mymedia.data.Item
@@ -11,6 +10,48 @@ import com.example.mymedia.R
 import com.example.mymedia.databinding.VideoItemBinding
 
 class HomeVideoListAdapter : ListAdapter<Item, HomeVideoListAdapter.ViewHolder>(ItemDiffCallback()) {
+
+    // 롱클릭 리스너 인터페이스 정의
+    interface OnItemLongClickListener {
+        fun onItemLongClick(item: Item)
+    }
+
+    private var onItemLongClickListener: OnItemLongClickListener? = null
+
+    // 롱클릭 리스너 설정 메서드
+    fun setOnItemLongClickListener(listener: OnItemLongClickListener) {
+        this.onItemLongClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            VideoItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    inner class ViewHolder(
+        private val binding: VideoItemBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Item) {
+            with(binding) {
+                posterImageView.setImageResource(R.drawable.img_netflix)
+
+                itemView.setOnLongClickListener {
+                    onItemLongClickListener?.onItemLongClick(item)
+                    true
+                }
+            }
+        }
+    }
+}
+
+class HomeChannelListAdapter : ListAdapter<Item, HomeChannelListAdapter.ViewHolder>(ItemDiffCallback()) {
 
     // 롱클릭 리스너 인터페이스 정의
     interface OnItemLongClickListener {
