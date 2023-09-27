b9b036acdaaa333fc54af1f019f39cd0ea796e7e
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-09-27 21:26:24 +0900

    FEAT:디테일 페이지 기능 구현 중

--- app/src/main/java/com/example/mymedia/home/HomeViewModel.kt
+++ app/src/main/java/com/example/mymedia/home/HomeViewModel.kt
@@ -2,11 +2,13 @@ package com.example.mymedia.home
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.mymedia.data.Category
 import com.example.mymedia.data.ChannelItem
 import com.example.mymedia.data.Data
 import com.example.mymedia.data.ItemRepository
@@ -33,10 +35,16 @@ class HomeViewModel(
     val most: LiveData<MutableList<VideoItem>>
         get() = _most
 
+    private val _categoryList = MutableLiveData<MutableList<Category>>()
+
+    val categoryList: LiveData<MutableList<Category>>
+        get() = _categoryList
+
     init {
         _categoryVideo.value = Data.getMediaData().filterIsInstance<VideoItem>().toMutableList()
         _categoryChannel.value = Data.getMediaData().filterIsInstance<ChannelItem>().toMutableList()
         _most.value = Data.getSearchData()
+        getCategoryList()
     }
 
     fun showDetail(videoItem: VideoItem, context: Context) {
@@ -44,10 +52,8 @@ class HomeViewModel(
         intent.putExtra("videoThumbnail", videoItem.thumbnail)
         intent.putExtra("videoTitle", videoItem.title)
         intent.putExtra("videoDescription", videoItem.description)
-
         context.startActivity(intent)
     }
-
     fun searchMostVideo() {
         viewModelScope.launch {
             val list = mutableListOf<MediaItem>()
@@ -64,11 +70,11 @@ class HomeViewModel(
         }
     }
 
-    fun searchByCategory() {
+    fun searchByCategory(id: String) {
         viewModelScope.launch {
             val list = mutableListOf<MediaItem>()
             // Video
-            val responseVideo = repository.findItemByCategory()
+            val responseVideo = repository.findItemByCategory(id)
             if (responseVideo.isSuccessful) {
                 val itemList = responseVideo.body() ?: mutableListOf()
                 list.addAll(itemList)
@@ -79,6 +85,22 @@ class HomeViewModel(
             }
             _categoryVideo.value = list.filterIsInstance<VideoItem>().toMutableList()
             _categoryChannel.value = list.filterIsInstance<ChannelItem>().toMutableList()
+        }
+    }
+
+    fun getCategoryList() {
+        viewModelScope.launch {
+            val list = mutableListOf<Category>()
+            // Video
+            val responseCategoryList = repository.findCategoryList()
+            if (responseCategoryList.isSuccessful) {
+                val categoryList = responseCategoryList.body() ?: mutableListOf()
+                list.addAll(categoryList)
+            } else {
+                // null일 시 공백 리스트 생성
+                _categoryList.value = mutableListOf()
+            }
+            _categoryList.value = list
         }
     }
 }
