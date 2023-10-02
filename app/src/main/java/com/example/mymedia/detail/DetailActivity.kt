30cbab0f50b494a44cddc446a59f93fabfc6b0ca
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-10-02 17:19:24 +0900

    FEAT:화면전환 애니메이션 추가

--- app/src/main/java/com/example/mymedia/detail/DetailActivity.kt
+++ app/src/main/java/com/example/mymedia/detail/DetailActivity.kt
@@ -1,9 +1,12 @@
 package com.example.mymedia.detail
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.mymedia.R
 import com.example.mymedia.data.VideoItem
 import com.bumptech.glide.Glide
@@ -15,12 +18,18 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        val rootLayout = findViewById<ConstraintLayout>(R.id.detailRootLayout)
         val thumbnailImageView: ImageView = findViewById(R.id.thumbnailImageView)
         val titleTextView: TextView = findViewById(R.id.videoTitleTextView)
         val descriptionTextView: TextView = findViewById(R.id.videoDescriptionTextView)
         val backButton: ImageView = findViewById(R.id.imageButton2)
 
+        val enterAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_up)
+        rootLayout.startAnimation(enterAnimation)
+
         backButton.setOnClickListener {
+            val exitAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_down)
+            rootLayout.startAnimation(exitAnimation)
             onBackPressed()
         }
 
@@ -36,4 +45,5 @@ class DetailActivity : AppCompatActivity() {
             .load(getHighQualityThumbnailUrl(imageUrl))
             .into(thumbnailImageView)
     }
+
 }
