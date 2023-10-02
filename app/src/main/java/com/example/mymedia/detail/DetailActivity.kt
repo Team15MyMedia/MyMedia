be027acab47e9f53aa403e1dd416b16cbd4dc949
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-10-02 20:06:10 +0900

    FEAT:공유기능 추가

--- app/src/main/java/com/example/mymedia/detail/DetailActivity.kt
+++ app/src/main/java/com/example/mymedia/detail/DetailActivity.kt
@@ -1,14 +1,14 @@
 package com.example.mymedia.detail
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.animation.Animation
+import android.util.Log
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.mymedia.R
-import com.example.mymedia.data.VideoItem
 import com.bumptech.glide.Glide
 import com.example.mymedia.home.getHighQualityThumbnailUrl
 
@@ -44,6 +44,30 @@ class DetailActivity : AppCompatActivity() {
         Glide.with(this)
             .load(getHighQualityThumbnailUrl(imageUrl))
             .into(thumbnailImageView)
+
+        val shareButton: ImageView = findViewById(R.id.imageButton4)
+        shareButton.setOnClickListener {
+            val thumbnailUrl = intent.getStringExtra("videoThumbnail")
+            shareVideo(thumbnailUrl)
+        }
     }
 
+    private fun shareVideo(thumbnailUrl: String?) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+
+        val videoId = extractVideoIdFromThumbnailUrl(thumbnailUrl)
+        val videoUrl = "https://www.youtube.com/watch?v=$videoId"
+
+        intent.putExtra(Intent.EXTRA_TEXT, videoUrl)
+
+        val shareIntent = Intent.createChooser(intent, "비디오 공유")
+        startActivity(shareIntent)
+    }
+
+    private fun extractVideoIdFromThumbnailUrl(thumbnailUrl: String?): String {
+        val pattern = "https://i.ytimg.com/vi/(.*?)/default.jpg".toRegex()
+        val matchResult = pattern.find(thumbnailUrl ?: "")
+        return matchResult?.groupValues?.get(1) ?: ""
+    }
 }
