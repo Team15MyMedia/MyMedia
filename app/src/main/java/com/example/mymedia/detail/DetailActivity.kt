c76f49fa89dcb32ca987a6e5b681fdcafe114530
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-10-04 15:30:16 +0900

    FEAT : 디테일 페이지에서 동영상 재생

--- app/src/main/java/com/example/mymedia/detail/DetailActivity.kt
+++ app/src/main/java/com/example/mymedia/detail/DetailActivity.kt
@@ -7,13 +7,19 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.mymedia.R
-import com.bumptech.glide.Glide
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
 class DetailActivity : AppCompatActivity() {
+
+    private var videoId: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         val rootLayout = findViewById<ConstraintLayout>(R.id.detailRootLayout)
-        val thumbnailImageView: ImageView = findViewById(R.id.thumbnailImageView)
+        val youtubePlayerView: YouTubePlayerView = findViewById(R.id.thumbnailImageView)
         val titleTextView: TextView = findViewById(R.id.videoTitleTextView)
         val descriptionTextView: TextView = findViewById(R.id.videoDescriptionTextView)
         val backButton: ImageView = findViewById(R.id.imageButton2)
@@ -33,24 +39,33 @@ class DetailActivity : AppCompatActivity() {
         titleTextView.text = title
         descriptionTextView.text = description
 
-        Glide.with(this)
-            .load(imageUrl)
-            .into(thumbnailImageView)
+        videoId = extractVideoIdFromThumbnailUrl(imageUrl)
+
+        lifecycle.addObserver(youtubePlayerView)
+        youtubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+
+                videoId?.let { youTubePlayer.loadVideo(it, 0f) }
+            }
+        })
 
         val shareButton: ImageView = findViewById(R.id.imageButton4)
         shareButton.setOnClickListener {
-            shareVideo(imageUrl)
+            shareVideo(videoId)
         }
     }
-    private fun shareVideo(thumbnailUrl: String?) {
+
+    private fun shareVideo(videoId: String?) {
         val intent = Intent(Intent.ACTION_SEND)
         intent.type = "text/plain"
-        val videoId = extractVideoIdFromThumbnailUrl(thumbnailUrl)
+
         val videoUrl = "https://www.youtube.com/watch?v=$videoId"
         intent.putExtra(Intent.EXTRA_TEXT, videoUrl)
+
         val shareIntent = Intent.createChooser(intent, "비디오 공유")
         startActivity(shareIntent)
     }
+
     private fun extractVideoIdFromThumbnailUrl(thumbnailUrl: String?): String {
         val pattern = "https://i.ytimg.com/vi/(.*?)/maxresdefault.jpg".toRegex()
         val matchResult = pattern.find(thumbnailUrl ?: "")
